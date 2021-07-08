7199256032b1d890d01a08dc5cf27f4a9dc38663
이유정 <thisoz>
2021-07-08 21:27:44 +0900

    add login feature through Firebase

---
+++ src/components/login/login.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+
+const Login = ({ authService }) => {
+  const onLogin = (event) => {
+    authService //
+      .login(event.currentTarget.textContent)
+      .then(console.log);
+  };
+  return (
+    <section>
+      <Header />
+      <section>
+        <h1>login</h1>
+        <ul>
+          <li>
+            <button onClick={onLogin}>Google</button>
+          </li>
+          <li>
+            <button onClick={onLogin}>Github</button>
+          </li>
+        </ul>
+      </section>
+      <Footer />
+    </section>
+  );
+};
+
+export default Login;
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,10 +1,11 @@
 import firebase from 'firebase';
+import firebaseApp from './firebase';
 
 class AuthService {
   login(providerName) {
     // provicerName 구글, 페이스북, 트위터
-    const authProvider = new firebase.auth[`${providerName}AutoProvicder`]();
-    return firebase.auth().signInWithPopup(authProvider);
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    return firebaseApp.auth().signInWithPopup(authProvider);
   }
 }
 
